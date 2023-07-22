7e9dcf5a1731cc48c592acb82ff7792e71bd4e70
Gursimran <gurmsi1060@outlook.com>
2023-07-23 00:53:32 +0530

    Refactored Companies name

--- src/main/java/org/astoria/concentration.java
+++ src/main/java/org/astoria/concentration.java
@@ -6,7 +6,6 @@ public class concentration {
     public void getConcatenation(int[] nums) {
         int[] ans = new int[nums.length*2];
 
-
 //        for(int i=0;i<ans.length;i++){
 //            if(i<nums.length){
 //                ans[i] = nums[i];
