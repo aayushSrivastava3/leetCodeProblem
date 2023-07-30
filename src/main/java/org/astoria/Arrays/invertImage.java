5ca62412c0de2b0b383b369cd817bb51c5d881fb
aayush <aayush.work120@gmail.com>
2023-07-30 22:00:22 +0530

    Added comments to the code

--- src/main/java/org/astoria/Arrays/invertImage.java
+++ src/main/java/org/astoria/Arrays/invertImage.java
@@ -1,8 +1,5 @@
 package org.astoria.Arrays;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class invertImage {
     public int[][] flipAndInvertImage(int[][] image) {
         int indexI = 0;
@@ -11,9 +8,11 @@ public class invertImage {
             int[] ar = new int[arr.length];
             int index=0;
             for(int i : arr){
+                //Start filling from the end of the array
+                //If 0 the add 1 
                 if(i == 0){
                     ar[arr.length-1-index] = 1;
-                }else{
+                }else{  //Otherwise add 0
                     ar[arr.length-1-index] = 0;
                 }
                 index++;
--- src/main/java/org/astoria/Strings/splitString.java
+++ src/main/java/org/astoria/Strings/splitString.java
@@ -10,6 +10,8 @@ public class splitString {
         for(String str : words){
             StringBuilder sb = new StringBuilder();
             for(int i=0; i<str.length(); i++){
+
+                //Iterate till we reach the separator
                 if(str.charAt(i) == separator){
                     if(sb.length() !=0 ){
                         ans.add(sb.toString());
@@ -17,6 +19,7 @@ public class splitString {
                     sb.setLength(0);
                 }else sb.append(str.charAt(i));
 
+                //IF separator not found and we iterate till the end of the string
                 if(i == str.length()-1){
                     ans.add(sb.toString());
                 }
